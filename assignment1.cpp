525fe79b4f97655dfb384d54f45129354720e991
Tanner <tanner.fleming@gmail.com>
2022-01-28 23:02:07 -0500

    Final

--- assignment1.cpp
+++ assignment1.cpp
@@ -11,47 +11,86 @@ typedef struct primeValues
 {
     int sum;
     int total;
+    int lastTen[10];
 } primeValues;
 
-bitset<100000000> Primes;
-void SieveOfEratosthenes(int n)
+bitset<100000000> Primes = {0};
+
+void SieveOfEratosthenes(int l, int r)
 {
-    Primes[0] = 1;
-    for (int i = 3; i <= n; i += 2)
+    primeValues ret;
+
+    // Going by 2 on odds only because even numbers are not prime
+    for (int i = l; i <= r; i += 2)
     {
-        if (Primes[i / 2] == 0)
+        bool flag = 1;
+        for (int j = 2; j * j <= i; ++j)
         {
-            for (int j = 3 * i; j <= n; j += 2 * i)
-                Primes[j / 2] = 1;
+            if (i % j == 0)
+            {
+                flag = 0;
+                break;
+            }
+        }
+
+        if (flag)
+        {
+            Primes[i] = 1;
+            ret.sum += i;
+            ret.total++;
         }
     }
+    // return ret;
 }
 
-// Driver Code
 int main()
 {
     clock_t start, end;
-
-    int n = 100000000;
     unsigned int finalSum = 2;
     int count = 1;
     float time = 0.0;
+    int bigTen[9] = {0};
 
     start = clock();
 
-    SieveOfEratosthenes(n);
-    for (int i = 1; i <= n; i++)
+    thread th1(SieveOfEratosthenes, 3, 50000000);
+    thread th2(SieveOfEratosthenes, 50000001, 75000000);
+    thread th3(SieveOfEratosthenes, 75000001, 82500000);
+    thread th4(SieveOfEratosthenes, 82500001, 90000000);
+    thread th5(SieveOfEratosthenes, 90000001, 95000000);
+    thread th6(SieveOfEratosthenes, 95000001, 97500000);
+    thread th7(SieveOfEratosthenes, 97500001, 98750000);
+    thread th8(SieveOfEratosthenes, 98750000, 100000000);
+
+    th1.join();
+    th2.join();
+    th3.join();
+    th4.join();
+    th5.join();
+    th6.join();
+    th7.join();
+    th8.join();
+
+    // While loop to find the 10 biggest primes
+    int loopCount = 0;
+    while (loopCount != 10)
     {
-        if (i % 2 == 1 && Primes[i / 2] == 0)
+        int i = 99999999;
+        if (Primes[i])
         {
-            finalSum += i;
-            count++;
+            bigTen[count] = i;
+            loopCount++;
         }
+        i -= 2;
     }
 
     end = clock();
 
     time = ((float)end - start) / CLOCKS_PER_SEC;
 
+    cout << "Execution Time: " << time << "secs"
+         << "\nSum of all primes found: " << finalSum 
+         << "\nNumber of primes found: " << count 
+         << "\nThe 10 biggest primes" << bigTen;
     return 0;
 }
